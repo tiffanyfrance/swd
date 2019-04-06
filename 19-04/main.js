b4b47a21fb2fc603ef9fa37cef5ed1036f0de819
tiffylou <tiffany6872@gmail.com>
2019-04-06 17:01:44 -0400

    hacky rwd

--- 19-04/main.js
+++ 19-04/main.js
@@ -29,77 +29,78 @@ function ratings(data) {
 
   let y = d3.scaleLinear()
             .range([height, 0]);
-            
+              
   let svg = d3.select(".viz").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", 
             "translate(" + margin.left + "," + (margin.top * 2) + ")");
+  
 
-    x.domain(data.map((d) => d.title));
-    y.domain([6, 9.5]);
+  x.domain(data.map((d) => d.title));
+  y.domain([6, 9.5]);
 
-    let tickArr = [6.5,7.0,7.5,8.0,8.5,9.0,9.5];
+  let tickArr = [6.5,7.0,7.5,8.0,8.5,9.0,9.5];
 
-    svg.append("g")
-      .call(d3.axisLeft(y)
-        .tickValues(tickArr)
-        .tickSizeInner(-width + margin.top - margin.bottom)
-        .tickSizeOuter(0)
-      );
+  svg.append("g")
+    .call(d3.axisLeft(y)
+      .tickValues(tickArr)
+      .tickSizeInner(-width + margin.top - margin.bottom)
+      .tickSizeOuter(0)
+    );
 
-    function bar(svg) {
-      svg.selectAll(".bar")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x", (d) => x(d.title))
-        .attr("width", x.bandwidth())
-        .attr("y", (d) =>  y(d.rating))
-        .attr("height", (d) => height - y(d.rating));
-    }
-
-    svg.selectAll(".title")
-        .data(data)
-        .enter()
-        .append('text')
-        .text((d) => `${d.title} \xa0 (${d.year})`)
-        .attr("x", (d) => x(d.title))
-        .attr("y", (d) => y(d.rating))
-        .attr('class', 'title')
-        .style("text-anchor", "end")
-        .attr('transform', (d) => `rotate(270,${x(d.title) - 1},${y(d.rating) + 4})`)
-        .style('font-size', '11px')
-        .style('fill', '#333');
-
-    const defs = svg.append('defs');
-    
-    defs.append('clipPath')
-      .attr('id', 'rect-clip1')
-      .call(bar);
-
-    svg.selectAll("image")
+  function bar(svg) {
+    svg.selectAll(".bar")
       .data(data)
       .enter()
-      .append('image')
-      .attr('xlink:href', (d) => `images/${d.image}`)
-      .attr('x', (d) => x(d.title))
-      .attr('y', 100)
-      // .attr('width', 512 + 'px')
-      .attr('height', 580 + 'px')
-      // // clip the image using id
-      .attr('clip-path', 'url(#rect-clip1)')
-      .style('cursor', 'pointer')
-      .on('click', function(d) {
-        let win = window.open(d.url, '_blank');
-        win.focus();
-      })
-      .on('mouseover', function(d) {
-        mouseOver(rollover,x,20,d);
-      })
-      .on('mouseout', mouseOut);
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => x(d.title))
+      .attr("width", x.bandwidth())
+      .attr("y", (d) =>  y(d.rating))
+      .attr("height", (d) => height - y(d.rating));
+  }
+
+  svg.selectAll(".title")
+      .data(data)
+      .enter()
+      .append('text')
+      .text((d) => `${d.title} \xa0 (${d.year})`)
+      .attr("x", (d) => x(d.title))
+      .attr("y", (d) => y(d.rating))
+      .attr('class', 'title')
+      .style("text-anchor", "end")
+      .attr('transform', (d) => `rotate(270,${x(d.title) - 1},${y(d.rating) + 4})`)
+      .style('font-size', '11px')
+      .style('fill', '#333');
+
+  const defs = svg.append('defs');
+  
+  defs.append('clipPath')
+    .attr('id', 'rect-clip1')
+    .call(bar);
+
+  svg.selectAll("image")
+    .data(data)
+    .enter()
+    .append('image')
+    .attr('xlink:href', (d) => `images/${d.image}`)
+    .attr('x', (d) => x(d.title))
+    .attr('y', 100)
+    // .attr('width', 512 + 'px')
+    .attr('height', 580 + 'px')
+    // // clip the image using id
+    .attr('clip-path', 'url(#rect-clip1)')
+    .style('cursor', 'pointer')
+    .on('click', function(d) {
+      let win = window.open(d.url, '_blank');
+      win.focus();
+    })
+    .on('mouseover', function(d) {
+      mouseOver(rollover,x,20,d);
+    })
+    .on('mouseout', mouseOut);
 
     axisLabel(svg,'IMDB Rating');
 
@@ -117,73 +118,73 @@ function length(data) {
 
   let y = d3.scaleLinear()
             .range([0, height]);
-            
+  
   let svg = d3.select('.viz').append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', 
-            'translate(' + margin.left + ',' + margin.top + ')');
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 
+              'translate(' + margin.left + ',' + margin.top + ')');
+        
+  x.domain(data.map((d) => d.title));
+  y.domain([40, 160]);
 
-    x.domain(data.map((d) => d.title));
-    y.domain([40, 160]);
+  let tickArr = [60,80,100,120,140];
+  
+  svg.append('g')
+    .call(d3.axisLeft(y)
+      .tickValues(tickArr)
+      .tickSizeInner(-width + margin.top - margin.bottom)
+      .tickSizeOuter(0)
+    );
 
-    let tickArr = [60,80,100,120,140];
-    
-    svg.append('g')
-      .call(d3.axisLeft(y)
-        .tickValues(tickArr)
-        .tickSizeInner(-width + margin.top - margin.bottom)
-        .tickSizeOuter(0)
-      );
-
-    function bar(svg) {
-      svg.selectAll('.bar2')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('class', 'bar2')
-        .attr('x', (d) => x(d.title))
-        .attr('width', x.bandwidth())
-        .attr('y', (d) => 0)
-        .attr('height', (d) => y(d.length));
-    }
-
-    const defs = svg.append('defs');
-    
-    defs.append('clipPath')
-      .attr('id', 'rect-clip2')
-      .call(bar);
-
-    svg.selectAll('image')
+  function bar(svg) {
+    svg.selectAll('.bar2')
       .data(data)
       .enter()
-      .append('image')
-      .attr('xlink:href', (d) => `images/${d.image}`)
+      .append('rect')
+      .attr('class', 'bar2')
       .attr('x', (d) => x(d.title))
-      .attr('y', -270)
-      // .attr('width', 512 + 'px')
-      .attr('height', 580 + 'px')
-      // // clip the image using id
-      .attr('clip-path', 'url(#rect-clip2)')
-      .style('cursor', 'pointer')
-      .on('click', function(d) {
-        let win = window.open(d.url, '_blank');
-        win.focus();
-      })
-      .on('mouseover', function(d) {
-        mouseOver(rollover,x,0,d);
-      })
-      .on('mouseout', mouseOut);
+      .attr('width', x.bandwidth())
+      .attr('y', (d) => 0)
+      .attr('height', (d) => y(d.length));
+  }
 
-    axisLabel(svg,'Movie length (min)')
+  const defs = svg.append('defs');
+  
+  defs.append('clipPath')
+    .attr('id', 'rect-clip2')
+    .call(bar);
 
-    addLabel(svg,98,330,35,35,-58,-12,'Longest Runtime');
-    addLabel(svg,850,255,48,48,-58,-10,'Most Popular on IMDB');
-    addLabel(svg,1143,322,40,40,-58,-10,'Most Votes in Poll');
+  svg.selectAll('image')
+    .data(data)
+    .enter()
+    .append('image')
+    .attr('xlink:href', (d) => `images/${d.image}`)
+    .attr('x', (d) => x(d.title))
+    .attr('y', -270)
+    // .attr('width', 512 + 'px')
+    .attr('height', 580 + 'px')
+    // // clip the image using id
+    .attr('clip-path', 'url(#rect-clip2)')
+    .style('cursor', 'pointer')
+    .on('click', function(d) {
+      let win = window.open(d.url, '_blank');
+      win.focus();
+    })
+    .on('mouseover', function(d) {
+      mouseOver(rollover,x,0,d);
+    })
+    .on('mouseout', mouseOut);
 
-    let rollover = svg.append('g')
-      .attr('class', 'rollover');
+  axisLabel(svg,'Movie length (min)')
+
+  addLabel(svg,98,330,35,35,-58,-12,'Longest Runtime');
+  addLabel(svg,850,255,48,48,-58,-10,'Most Popular on IMDB');
+  addLabel(svg,1143,322,40,40,-58,-10,'Most Votes in Poll');
+
+  let rollover = svg.append('g')
+    .attr('class', 'rollover');
 
 }
 
