cebb1d8d6497c96d9a6d53bf764b7f8699e51afa
tiffylou <tiffany6872@gmail.com>
2019-05-08 15:35:28 -0400

    updates

--- 19-05/main.js
+++ 19-05/main.js
@@ -6,10 +6,10 @@
 */
 
 const margin = {top: 10, right: 10, bottom: 20, left: 50},
-    width = 1280 - margin.left - margin.right,
-    height = 380 - margin.top - margin.bottom;
+    width = 380 - margin.left - margin.right,
+    height = 680 - margin.top - margin.bottom;
 
-d3.csv("data.csv", (error, data) => {
+d3.csv('data.csv', (error, data) => {
   if (error) throw error;
 
   data.forEach((d) => {
@@ -17,16 +17,37 @@ d3.csv("data.csv", (error, data) => {
     d.Donate = +d.Donate;
   });
 
-  buildRect(data);
+  sortData(data);
   console.log(data)
 });
 
-function buildRect(data) {
+function sortData(data) {
+  let keepData = [],
+      donateData = [];
+  
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].Keep === 1) {
+      keepData.push(data[i]);
+    } else {
+      donateData.push(data[i]);
+    }
+  }
+  
+  addRect(donateData, '#ccc');
+  addRect(keepData, 'red');
+}
+
+function addRect(data, color) {
   let svg = d3.select('#viz').append('svg')
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top * 2})`);
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('class', 'stack')
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top * 2})`);
+
+  let delta = Math.floor(data.length / 5);
+
+  console.log(delta)
 
   svg.selectAll('rect')
     .data(data)
@@ -34,15 +55,13 @@ function buildRect(data) {
     .append('rect')
     .attr('width', 20)
     .attr('height', 5)
-    .attr('fill', (d, i) => {
+    .attr('fill', (d) => {
       if (d['Spark joy'] === 1) {
         return 'gold';
-      } else if (d.Keep === 1) {
-        return 'red';
-      } else if (d.Keep === 0) {
-        return '#ccc';
+      } else {
+        return color;
       }
     })
-    .attr('x', (d, i) => 25 * i);
-
+    .attr('x', (d, i) => 25) 
+    .attr('y', (d, i) => Math.floor(i/5) * 10);
 }
